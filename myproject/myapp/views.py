e8e993d3061dc56d37b2f6ccd7b110d522269af6
SakibSamiul <sb.sakib77@gmail.com>
2024-09-19 22:24:10 +0600

    register About

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from . models import ABOUT
 
 # Create your views here.
 def Index(request):
     return render(request, 'index.html')
 
 def About(request):
-    return render(request, 'about.html')
+    about = ABOUT.objects.get()
+    return render(request, 'about.html', {'about':about})
 
 def Departments(request):
     return render(request, 'departments.html')
