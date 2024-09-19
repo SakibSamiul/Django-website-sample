9dbca504116c725c999256babfc28ae3e2bebba2
SakibSamiul <sb.sakib77@gmail.com>
2024-09-19 15:52:11 +0600

    create admin panel

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ABOUT(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to= 'ABOUT/')
